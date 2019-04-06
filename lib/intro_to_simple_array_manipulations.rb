0840761b2e1c2f805aa209030387b12302676eb1
Ayleen  Nazario <ayleen.nazario@gmail.com>
2019-04-06 20:22:07 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,39 @@
+def using_push (hello = ["hello", "hola", "bonjour"], portuguese = "oi")
+  hello.push(portuguese)
+end 
+
+def using_unshift
+  
+end 
+
+def using_push 
+  
+end 
+
+def using_push 
+  
+end 
+
+def using_push 
+  
+end 
+
+def using_push 
+  
+end 
+
+def using_push 
+  
+end 
+
+def using_push 
+  
+end 
+
+def using_push 
+  
+end 
+
+def using_push 
+  
+end 
